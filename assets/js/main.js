b6b3da4fb2f402abbaf54a93d292fc9f78184c9b
Saul Matías Jiménez <saulmaji@gmail.com>
2026-03-17 00:20:53 +0100

    Contacto y carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.mySwiper', {
-    loop: true,
-    spaceBetween: 10,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1200: {
-        slidesPerView: 3,
-      }
-    }
-  });
+    const swiper = new Swiper('.mySwiper', {
+        loop: true,
+        spaceBetween: 10,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 4,
+                loop: false,           // desactiva loop
+                allowTouchMove: false, // desactiva navegación táctil
+                autoplay: false        // desactiva autoplay
+            }
+        }
+    });
 });
